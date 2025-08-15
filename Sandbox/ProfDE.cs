8653eaf62b493749d5408ddb8f2c2a59203c9ed3
HughCoding <160680383+HughCoding@users.noreply.github.com>
2025-08-15 08:46:25 -0300

    Add files via upload

---
+++ Sandbox/ProfDE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class ProfDE : Professor
+
+    {
+        public override void calcularBeneficio()
+        {
+            Console.WriteLine($"seu beneficio é de: {getBeneficio}");
+        }
+
+
+
+    }
+}
---
+++ Sandbox/ProfHorista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    internal class ProfHorista : Professor
+    {
+        public double valorHora { get; set; }
+
+
+        public override void calcularBeneficio()
+        {
+            Console.WriteLine();
+        }
+       
+
+    }
+}
